9dce97745ead4e92253c5f383d9757fe84a585d8
Piotr Ryzak <s28771@pjwstk.edu.pl>
2024-03-12 11:28:41 +0100

    add GetAverage method

--- APBD/APBD/Program.cs
+++ APBD/APBD/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("modyfikacja gita");
 Console.WriteLine("modyfikacja gita 2 ");
 Console.WriteLine("modyfikacja gita 3 ");
+
+static double GetAverage(int[] numbers)
+{
+    int sum = 0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return (double)sum / numbers.Length;
+}
